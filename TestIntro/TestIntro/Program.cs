e6df034afcd7b56123aee2b90cda89014392a8af
roman-arushanov <roman.arushanov@gmail.com>
2018-07-03 21:02:43 +0400

    added codefights first solution

--- TestIntro/TestIntro/Program.cs
+++ TestIntro/TestIntro/Program.cs
@@ -8,9 +8,25 @@ namespace TestIntro
 {
 	class Program
 	{
+		static int add(int param1, int param2)
+		{
+			return param1 + param2;
+		}
 		static void Main(string[] args)
 		{
 
+			Console.WriteLine("Plece input first number");
+			var x = Console.ReadLine();
+			Console.WriteLine("Plece input second number");
+			var y = Console.ReadLine();
+             int xInt = int.MaxValue;
+			int.TryParse(x, out xInt);
+             int yInt = int.MaxValue;
+			int.TryParse(y, out yInt);
+			if (xInt == 0 || yInt == 0)
+				Console.WriteLine("Incorrect input");
+			else
+				Console.WriteLine(add(xInt, yInt));
 		}
 	}
 }
